f2d403647b17462e95c624bec9e3301247111240
Gabriel <gabrielfelipemoreiraj@gmail.com>
2025-06-01 23:31:49 -0300

    Adiciona fake js

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,29 @@
+// prisma/seed.ts
+import { PrismaClient } from '../generated/prisma'; // ou 'prisma/prisma-client' se estiver usando o caminho padrão
+import { faker } from '@faker-js/faker';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const usersData = Array.from({ length: 5 }).map(() => ({
+    email: faker.internet.email(),
+    name: faker.person.fullName(),
+    cpf: faker.string.numeric(11),
+  }));
+
+  await prisma.users.createMany({
+    data: usersData,
+    skipDuplicates: true,
+  });
+
+  console.log('✅ Usuários de teste criados');
+}
+
+main()
+  .catch(e => {
+    console.error('Erro ao criar usuários:', e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
