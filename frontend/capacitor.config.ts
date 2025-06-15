ed19531e994a5d835808ed6ae6140d706356966a
Gabriel <gabrielfelipemoreiraj@gmail.com>
2025-06-14 21:02:26 -0300

    Adiciona capacitor android

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'meu-app',
+  appName: 'Controle Financeiro',
   webDir: 'dist'
 };
 
