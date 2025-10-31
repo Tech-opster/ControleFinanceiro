9da781d7168bccbea426236790c9bb0d869101da
Tech-opster <gabrielfelipemoreiraj@gmail.com>
2025-10-30 21:31:33 -0300

    Corrige currentMonth

--- frontend/src/hooks/useCurrentMonth.ts
+++ frontend/src/hooks/useCurrentMonth.ts
@@ -4,15 +4,15 @@ export function useCurrentMonth<T extends { date: Date | string }>(data: T[]) {
     
   const currentMonthData = useMemo(() => {
     const now = new Date();
-    const currentMonth = now.getMonth();
-    const currentYear = now.getFullYear();
+    const currentMonth = now.getUTCMonth();
+    const currentYear = now.getUTCFullYear();
 
     return data.filter((item) => {
       const itemDate = new Date(item.date);
       if (Number.isNaN(itemDate.getTime())) return false; // valida
       return (
-        itemDate.getMonth() === currentMonth &&
-        itemDate.getFullYear() === currentYear
+        itemDate.getUTCMonth() === currentMonth &&
+        itemDate.getUTCFullYear() === currentYear
       );
     });
   }, [data]);
